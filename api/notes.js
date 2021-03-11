54b0c69958dd13a96121afeada95bb2523d758a2
OxyTom <oxytom8@gmail.com>
2021-03-11 12:17:59 +0100

    dcdfsfqs

---
+++ api/notes.js
@@ -0,0 +1,24 @@
+const initRoutes = (app, db) => {
+    /*
+app.post('/notes', (req, res) => {
+    students.push(req.body);
+    res.status(200).json(students);
+});
+ 
+app.put('/notes/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const student = students.find(stud => stud.id === id);
+    student = req.body;
+    res.status(200).json(students);
+});
+ 
+app.delete('/notes/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const student = students.find(stud => stud.id === id);
+    students.splice(students.indexOf(student), 1);
+    res.status(200).json(students);
+})
+    */
+}
+
+exports.initRoutes = initRoutes;
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(express.json());
 require('./api/students').initRoutes(app, db);
 require('./api/evaluators').initRoutes(app, db);
 require('./api/projects').initRoutes(app, db);
+require('./api/notes').initRoutes(app, db);
 
 app.listen(8080, () => {
     console.log("Server ready");
