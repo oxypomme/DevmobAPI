69b7b89adefe7d681bee73ba1ab9a7a523741fe2
OxyTom <oxytom8@gmail.com>
2021-03-11 12:14:55 +0100

    added a new way to get evaluators

--- api/evaluators.js
+++ api/evaluators.js
@@ -1,4 +1,11 @@
 const initRoutes = (app, db) => {
+    app.get('/evaluateurs/:name', (req, res) => {
+        const { name } = req.params;
+        db.con.query(`SELECT id_evaluateur, nom, prenom FROM evaluateur WHERE nom='${name}'`, (err, data) => {
+            res.status(200).json(data);
+        });
+    })
+
     app.get('/evaluateurs/:mail/:pass', (req, res) => {
         const { mail, pass } = req.params;
         db.con.query(`SELECT id_evaluateur, nom, prenom, email FROM evaluateur WHERE email='${mail}' AND motdepasse='${pass}'`, (err, data) => {
